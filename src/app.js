7f7371f605b6e2cfbcc331e076b2968ab2cd17e0
Donovan Chavez <chavezdon17@gmail.com>
2021-01-30 20:52:18 -0600

    import tables and API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import React from "react";
+import userInfo from "./API/API.js";
+import Tables from "./components/Tables/tables.js";
 
-function App() {}
+function App() {
+  return <Tables employees={userInfo}> </Tables>;
+}
 
 export default App;
